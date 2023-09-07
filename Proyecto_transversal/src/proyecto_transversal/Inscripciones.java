994bcf0f7571f660272e253b1b44b5681093af5c
eliastre72 <111254521+eliastre72@users.noreply.github.com>
2023-09-07 16:58:28 -0300

    Entidades. Inscripcion
    con 4 constructores

---
+++ Proyecto_transversal/src/proyecto_transversal/Inscripciones.java
@@ -0,0 +1,71 @@
+
+package proyecto_transversal;
+
+
+public class Inscripciones {
+    
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripciones() {
+    }
+
+    public Inscripciones(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripciones(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripciones(double nota) {
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripciones{" + "idInscripcion=" + idInscripcion + ", alumno=" + alumno + ", materia=" + materia + ", nota=" + nota + '}';
+    }
+    
+    
+    
+}
