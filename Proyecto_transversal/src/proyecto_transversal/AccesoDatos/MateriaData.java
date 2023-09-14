b7af52a7b7b63b017cec2508134139caac3a0658
eliastre72 <111254521+eliastre72@users.noreply.github.com>
2023-09-14 15:31:24 -0300

    materiaData
    Terminado los 3 metodos faltantes

--- Proyecto_transversal/src/proyecto_transversal/AccesoDatos/MateriaData.java
+++ Proyecto_transversal/src/proyecto_transversal/AccesoDatos/MateriaData.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -44,13 +46,13 @@ public class MateriaData {
     
     public Materia buscarMateria(int id){
         Materia materia = null;
-        String sql = "SELECT nombre, año, estado FROM materia WHERE idMateria = " + materia.getIdMateria()+"AND estado = 1";
+        String sql = "SELECT nombre, año, estado FROM materia WHERE idMateria = ? AND estado = 1";
         
         PreparedStatement ps = null;
         
         try{
             ps = con.prepareStatement(sql);
-            
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
                 materia = new Materia(id, rs.getString("nombre"),rs.getInt("año"), rs.getBoolean("estado"));
@@ -65,5 +67,69 @@ public class MateriaData {
         }
         
         return materia;
+    }
+    
+    public void modificarMateria(Materia materia){
+        String sql = "UPDATE materia SET nombre = ?, año = ?, estado = ? WHERE idMateria = ?";
+        PreparedStatement ps = null;
+        
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, materia.getIdMateria());
+            ps.setString(2, materia.getNombre());
+            ps.setInt(3, materia.getAnioMateria());
+            ps.setBoolean(4, materia.isEstado());
+            int exito = ps.executeUpdate();
+            
+            if(exito == 1){
+                 JOptionPane.showMessageDialog(null, "Modificado exitosamente.");
+            }else{
+                 JOptionPane.showMessageDialog(null, "La materia no existe");
+            }
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ ex.getMessage());
+        }
+    }
+    
+    public void eliminarMateria(int id){
+        try{
+            String sql = "UPDATE materia SET  estado = 0 WHERE idMateria = ?" ;
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+            
+            if(fila == 1){
+                JOptionPane.showMessageDialog(null, "Se elimino la materia");
+            }
+            ps.close();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ ex.getMessage());
+        }
+    }
+
+
+    public List<Materia> listarMaterias(){
+        List<Materia> materias = new ArrayList<>();
+        try{
+            String sql = "SELECT * FROM materia WHERE estado = 1";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Materia  materia = new Materia();
+                
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("año"));
+                materia.setEstado(rs.getBoolean("estado"));
+                materias.add(materia);
+            }
+            ps.close();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia"+ ex.getMessage());
+        }
+        return materias;
     }
 }
--- Proyecto_transversal/src/proyecto_transversal/Main.java
+++ Proyecto_transversal/src/proyecto_transversal/Main.java
@@ -9,7 +9,9 @@ import java.time.LocalDate;
 import java.time.Month;
 import proyecto_transversal.AccesoDatos.AlumnoData;
 import proyecto_transversal.AccesoDatos.Conexion;
+import proyecto_transversal.AccesoDatos.MateriaData;
 import proyecto_transversal.Entidades.Alumno;
+import proyecto_transversal.Entidades.Materia;
 
 /**
  *
@@ -23,9 +25,15 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("holaaaaaaaaa");
         
-        Alumno alumno = new Alumno(12345678, "Suarez", "Alan", LocalDate.of(1999, Month.MARCH, 12), true);
-        AlumnoData alumnodata = new AlumnoData();
-        alumnodata.guardarAlumno(alumno);
+      //  Alumno alumno = new Alumno(12345678, "Suarez", "Alan", LocalDate.of(1999, Month.MARCH, 12), true);
+       // AlumnoData alumnodata = new AlumnoData();
+       // alumnodata.guardarAlumno(alumno);
+        
+        Materia materia = new Materia("Historia Universal", 2001,true);
+        MateriaData materiadata = new MateriaData();
+        materiadata.buscarMateria(5);
+       // materiadata.guardarMateria(materia);
+        
     }
     
 }
